6bbbc96819210019bbdcd99ac26a45b433f9c7df
Nikhil <support@taytelar.com>
2024-09-03 16:55:40 +0530

    topbar & header responsive

--- TayTelar/src/components/header/Header.tsx
+++ TayTelar/src/components/header/Header.tsx
@@ -7,30 +7,60 @@ import "../../assets/sass/components/_header.scss";
 import logo from "../../assets/images/logo.png";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useNavigate } from "react-router-dom";
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import ClearSharpIcon from '@mui/icons-material/ClearSharp';
+import { useState } from "react";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleCartClick = () => {
     navigate("/cart");
   };
 
+  const toggleMenu = () => {
+    const centerMenu = document.querySelector('.center');
+  
+    if (centerMenu) {
+      if (menuOpen) {
+        centerMenu.classList.add('closing');
+  
+        setTimeout(() => {
+          setMenuOpen(false);
+          centerMenu.classList.remove('open', 'closing');
+          document.body.style.overflow = "auto";
+        }, 300);
+  
+      } else {
+        setMenuOpen(true);
+        document.body.style.overflow = "hidden";
+      }
+    }
+  };
+  
   return (
     <>
       <TopBar />
       <nav>
-        <div className="menu_list">
+        <div className="menu_list" >
           <div className="menu_list_container">
-            <div className="menu_list_container_section">
+            <div className={`menu_list_container_section ${menuOpen ? "menu-overlay" : ""}`}>
+              <div className={`hamburger`} onClick={toggleMenu}>
+                <MenuSharpIcon />
+                <SearchIcon className="searchicon" />
+              </div>
               <div className="logo_left">
                 <img src={logo} alt="LOGO" />
               </div>
-              <div className="center">
+              <div className={`center ${menuOpen ? "open" : ""}`}>
                 <ul>
+                  <li><p>Menu</p><ClearSharpIcon className="x-mark" onClick={toggleMenu}/></li>
                   <li>
                     <NavLink
                       to="/home"
                       className={({ isActive }) => (isActive ? "active" : "")}
+                      onClick={toggleMenu}
                     >
                       Home
                     </NavLink>
@@ -47,6 +77,7 @@ const Header = () => {
                     <NavLink
                       to="/aboutUs"
                       className={({ isActive }) => (isActive ? "active" : "")}
+                      onClick={toggleMenu}
                     >
                       Collections <ExpandMoreIcon />
                     </NavLink>
@@ -55,6 +86,7 @@ const Header = () => {
                     <NavLink
                       to="/blogs"
                       className={({ isActive }) => (isActive ? "active" : "")}
+                      onClick={toggleMenu}
                     >
                       Blogs <ExpandMoreIcon />
                     </NavLink>
@@ -63,6 +95,7 @@ const Header = () => {
                     <NavLink
                       to="/contactUs"
                       className={({ isActive }) => (isActive ? "active" : "")}
+                      onClick={toggleMenu}
                     >
                       Contact Us
                     </NavLink>
@@ -71,18 +104,17 @@ const Header = () => {
               </div>
               <div className="list_right">
                 <ul>
-                  {" "}
                   <li>
                     <NavLink
                       to="/login"
                       className={({ isActive }) => (isActive ? "active" : "")}
                     >
                       <PermIdentityIcon />
-                      &nbsp; Login / Register
+                      &nbsp; <p>Login / Register</p>
                     </NavLink>
                   </li>
                   <li>
-                    <SearchIcon />
+                    <SearchIcon className="searchicon" />
                   </li>
                   <li>
                     <LocalMallIcon onClick={handleCartClick} />
